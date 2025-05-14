eb37b1e641e210b09b2872b34a70c865ef73a29a
avidson <marcusjobng@gmail.com>
2025-05-14 11:00:53 +0100

    may14th

--- src/Pages/Store.jsx
+++ src/Pages/Store.jsx
@@ -34,12 +34,11 @@ function Store({ setSelectedCard }) {
         <StoreCategory />
 
             <div className="bg-main-color pad1 py-8 gap-8 text-white flex flex-col items-center justify-center">
-                    <h1 className='text-[32px] tablet:text-[28px] phone:text-[20px]'>Coming Soon!</h1>
         
-                    <h3 className='text-[26px] tablet:text-[18px]'>Store Will Soon Be Open For All!</h3>
+                    <h3 className='text-[26px] tablet:text-[18px]'>For Fast Order Placement!</h3>
         
                     <p className='text-center'>
-                           For More Info, Please Contact: 
+                           Please Contact: 
                     </p>
                     <p className='text-center'>
                            Nwanneka Lynda Chukwurah: +1 (614) 369 9147 
